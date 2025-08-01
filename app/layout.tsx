750f6c8c84055b3749fae66030ec9e95017b56bc
Mush2014 <mac-m2@mush.com>
2025-08-01 15:10:38 +0800

    add clarity 统计

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,20 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
             gtag('config', 'G-8FLR53176S');
           `}
             </Script>
+            <Script id="clarity" strategy="afterInteractive">
+                {`
+            (function (c, l, a, r, i, t, y) {
+              c[a] = c[a] || function () {
+                (c[a].q = c[a].q || []).push(arguments)
+              };
+              t = l.createElement(r);
+              t.async = 1;
+              t.src = "https://www.clarity.ms/tag/" + i;
+              y = l.getElementsByTagName(r)[0];
+              y.parentNode.insertBefore(t, y);
+            })(window, document, "clarity", "script", "snwq484iba");
+          `}
+            </Script>
         </head>
         <body className="bg-white dark:bg-black min-h-screen">
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
